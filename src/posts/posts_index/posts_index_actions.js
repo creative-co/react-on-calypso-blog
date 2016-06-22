899a9a3b52e37ba43ea92a1f19a158a7443afea7
Ivan Efremov <st8998@gmail.com>
2016-06-22 19:12:12 +0400

    tipo fix

--- src/posts/posts_index/posts_index_actions.js
+++ src/posts/posts_index/posts_index_actions.js
@@ -7,7 +7,7 @@ export const add = (posts, page = 0) => ({type: 'POSTS_INDEX.ADD', posts, page})
 export const replace = (posts) => ({type: 'POSTS_INDEX.REPLACE', posts})
 
 export const request = (params = {}) => dispatch =>
-  api.blog.postsList(merge({ fields: 'ID,date,author,slug,title,excerpt,tags,likes_count' }, params))
+  api.blog.postsList(merge({ fields: 'ID,date,author,slug,title,excerpt,tags,like_count' }, params))
     .then(res => {
       dispatch(addPosts(res.posts))
       dispatch(params.page === 0 ? replace(res.posts) : add(res.posts, params.page))
