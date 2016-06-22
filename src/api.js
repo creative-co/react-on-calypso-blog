4b4fba8fba324137f5e4f549c8dc392ed68ff9b8
Vladimir Yartsev <vovayartsev@gmail.com>
2016-06-22 17:50:25 +0400

    GF pages

--- src/api.js
+++
@@ -1,7 +0,0 @@
-import wpcom from 'wpcom'
-
-export const siteUrl = 'inasearchofbalance.wordpress.com'
-export const wp = wpcom()
-export const blog = wp.site(siteUrl)
-
-export default { siteUrl, wp, blog }
